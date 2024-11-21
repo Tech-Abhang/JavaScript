33ef44322f926fb9dc1f8596bc8f8f044765d967
Abhang Pawar <abhangpawar03@gmail.com>
2024-11-21 21:40:59 +0530

    added comments

--- Project/guessgame.js
+++ Project/guessgame.js
@@ -2,10 +2,12 @@
 //write a js program to generate a random number and store it in a variable . The progra, then takes an input from the user to tell them whether guesss is correct or not.
 
 const number1=Math.floor(Math.random() *100);  // to print number between 1-100
-console.log(number1)
 let score = 100 ;
+
 while (true) {
+
     let guess = prompt("Enter your guess : ");
+
     if (guess == number1){
         console.log(`You guessed the right number ${number1}`);
         console.log(`Your socre is ${score}`);
@@ -18,8 +20,17 @@ while (true) {
         break
     }
 
-    else{
-        console.log("You guessed the wrong number");
+    else if( guess > number1 && guess <100 ){
+        console.log("go lower");
         score-=1;
     }
+
+    else if( guess < number1 && guess >0 ){
+        console.log("go higher");
+        score-=1;
+    }
+
+    else{
+        console.log("enter a number between 1-100")
+    } 
 }
