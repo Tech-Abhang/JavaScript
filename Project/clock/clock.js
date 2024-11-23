f022a96fa8cc37fa17c7d8cc9c1cf106b6107d9c
Abhang Pawar <abhangpawar03@gmail.com>
2024-11-23 23:56:18 +0530

    added comments

--- Project/clock/clock.js
+++ Project/clock/clock.js
@@ -4,18 +4,26 @@ setInterval( () =>{
     
     let d = new Date();
     
+    // get hours , minutes,seconds from d
     let h = d.getHours();
     let m = d.getMinutes();
     let s = d.getSeconds();
 
+    // making a string for time
     const timeString = `${h} : ${m} : ${s}`;
+
+    // adding the string to div with index
     t.textContent = timeString;
 
+    // get date , month , year from d
     const day = d.getDate();
     const month = d.getMonth();
     const year = d.getFullYear();
 
-    const dateString = `${day} / ${month} / ${year}`
+    // create a string for date
+    const dateString = `${day} / ${month} / ${year}`;
+
+    // adding the string to div with index
     dateElement.textContent = dateString;  
 
 },1000)
